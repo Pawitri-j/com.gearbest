927fb33a51714e8ef2ff94117553d50bbea2e633
Pawittri Jaisupap <pawitri.j@gmail.com>
2022-06-28 06:35:31 -0400

    aaaaaa

--- src/test/java/pageFactory/ProductPage.java
+++ src/test/java/pageFactory/ProductPage.java
@@ -9,21 +9,28 @@ import org.openqa.selenium.support.PageFactory;
 import utilities.BaseClass;
 import utilities.CommonMethods;
 
-public class ProductPage extends CommonMethods{
+public class ProductPage extends CommonMethods {
 
-public ProductPage() {
-		
-		PageFactory.initElements(BaseClass.getDriver(), this);	
-		
+	public ProductPage() {
+
+		PageFactory.initElements(BaseClass.getDriver(), this);
+
 	}
+
+//Pawitri 06/28/22
+	@FindBy(xpath = "//*[@id=\"siteWrap\"]/div[1]/div[1]/div/div[2]/ul/li")
+	public List<WebElement> productList;
+
+	@FindBy(xpath = "//*[@id='goodsDetail']/div[2]/div[1]/div[2]/div[5]/a[1]")
+	public WebElement addToCartButton;
+	
+	
+	
+	
+	
+	
+	
+	
 	
 
-  @FindBy(xpath = "//*[@id=\"siteWrap\"]/div[1]/div[1]/div/div[2]/ul/li") 
-  public List<WebElement> productList;
-  
-  @FindBy(xpath = "//*[@id='goodsDetail']/div[2]/div[1]/div[2]/div[5]/a[1]") 
-  public WebElement addToCartButton;
-
-	
-
-}//class
+}// class
