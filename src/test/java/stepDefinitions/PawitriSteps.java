5903d81c381546e2f47a1d7d57ba5a65d19c8f58
Pawittri Jaisupap <pawitri.j@gmail.com>
2022-06-28 02:41:00 -0400

    gearbest

--- src/test/java/stepDefinitions/PawitriSteps.java
+++ src/test/java/stepDefinitions/PawitriSteps.java
@@ -7,23 +7,62 @@ import utilities.CommonMethods;
 
 public class PawitriSteps extends CommonMethods{
 
+
+	@Given("Click each Tab on Home page1")
+	public void click_each_tab_on_home_page1() {
+	  
+	}
 	
 	
+	@When("Click on Cart Icon button from that page1")
+	public void click_on_cart_icon_button_from_that_page1() {
+
+	}
+	
+	
+	@Then("User should navigate to the Cart page after clicking the Cart icon button1")
+	public void user_should_navigate_to_the_cart_page_after_clicking_the_cart_icon_button1() {
+	    
+	}
 
 
-	@Given("Click Tab on Home page to product page")
-	public void click_tab_on_home_page_to_product_page() {
-	System.out.println("Hellooooooooooooooooooooooooooo11111111111111");
+//-------------------------------------------------------------------------------------------------//
+
+	@Given("Click Tab on Home page to product page2")
+	public void click_tab_on_home_page_to_product_page2() {
+	  
 	}
+
+	@When("Click the chosen product and click Add to cart button2")
+	public void click_the_chosen_product_and_click_add_to_cart_button2() {
+	  
+	}
+
+	@When("Click Cart Icon button1")
+	public void click_cart_icon_button1() {
 	
-	@When("Select product and Click Add to cart")
-	public void select_product_and_click_add_to_cart() {
-		System.out.println("Hellooooooooooooooooooooooooooo222222222222222");
 	}
-	@Then("Verify User able to change the quantity of product in the Cart")
-	public void verify_user_able_to_change_the_quantity_of_product_in_the_cart() {
-		System.out.println("Hellooooooooooooooooooooooooooo3333333333333333333333333");
+
+	@Then("Verify User able to change the quantity of product in the Cart2")
+	public void verify_user_able_to_change_the_quantity_of_product_in_the_cart2() {
+	   
 	}
+//-------------------------------------------------------------------------------------------------//
+
+
+	@Given("Click Cart icon button3")
+	public void click_cart_icon_button3() {
+	  ;
+	}
+	@When("User should be able to see and click Go Shopping button3")
+	public void user_should_be_able_to_see_and_click_go_shopping_button3() {
+	   
+	}
+	@Then("the cart is empty which navigates to the homepage3")
+	public void the_cart_is_empty_which_navigates_to_the_homepage3() {
+	   
+	}
+
 
 
 
