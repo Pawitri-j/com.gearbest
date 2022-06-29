3f6858cd23350567ba797a1119848c0c54347028
TahirM786 <100324846+TahirM786@users.noreply.github.com>
2022-06-29 17:03:35 -0400

    maria

--- src/test/java/stepDefinitions/MariaSteps.java
+++ src/test/java/stepDefinitions/MariaSteps.java
@@ -1,7 +1,53 @@
 package stepDefinitions;
 
+import org.junit.Assert;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import utilities.BaseClass;
 import utilities.CommonMethods;
 
 public class MariaSteps extends CommonMethods {
 
+	@Given("User clicks searchIcon")
+	public void user_clicks_search_icon() {
+		h.searchInput.click();
+	
+	}
+
+   @Given("User is on Home Page and inputs {string}")
+   public void user_is_on_home_page_and_inputs(String string) {
+	   
+	  h.searchByWord();
+	   
+   
 }
+   							////scenario 2/////
+	@Then("Verify User should be able to see search list of searchItem")
+	public void verify_user_should_be_able_to_see_search_list_of_search_item() {
+		 h.searchWordInputSuggestionList();
+	}
+	
+	
+	@Given("User is on Home Page")
+	public void user_is_on_home_page() {
+	   String currentUrl=BaseClass.getDriver().getCurrentUrl();
+	   Assert.assertTrue(currentUrl.equals(BaseClass.getPropertyString("homePageURL")));
+	}
+
+	@Given("User clicks All button on searchBar")
+	public void user_clicks_all_button_on_search_bar() {
+	    h.clickingSearchallBtn();
+	}
+
+	@Given("User clicks on each Tab in Options list")
+	public void user_clicks_on_each_tab_in_options_list()  {
+	  h.searchByAllButtonListOptions();
+	}
+
+	@Then("Verify User should be able to navigate to correspondant tab page")
+	public void verify_user_should_be_able_to_navigate_to_correspondant_tab_page() {
+	   h.searchTitleverification();
+	}	
+	
+}//class
